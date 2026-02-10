aa5ddfb73c78356cac421439188954c2d90dc7da
ashishsinghk2503 <ashishsinghk2503@gmail.com>
2026-02-10 07:05:38 +0530

    Added dark mode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,17 @@
-import { Moon, Terminal } from "lucide-react";
-import React from "react";
+import { Moon, Sun, Terminal } from "lucide-react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { Button } from "./ui/button";
 
 const Header = () => {
+  const [isDark, setIsDark] = useState(true);
+
+  const toogleTheme = () => {
+    setIsDark(!isDark);
+    document.documentElement.classList.toggle("dark")
+  };
   return (
-    <div className="flex items-center justify-between">
+    <div className="flex items-center justify-around">
       <div className="flex items-center gap-3 p-5">
         <div className="w-14 h-14 bg-indigo-600 rounded-2xl flex items-center justify-center cursor-pointer">
           <Terminal className="w-8 h-8 text-white" />
@@ -12,7 +19,7 @@ const Header = () => {
         <span className="text-md font-semibold cursor-pointer">ZenflowJS</span>
       </div>
 
-      <div className="flex items-center gap-8 ml-auto">
+      <div className="flex items-center gap-8 ">
         <ul className="flex gap-6">
           <Link
             to=""
@@ -41,7 +48,9 @@ const Header = () => {
         </ul>
 
         <div className="flex items-center gap-4 pr-5">
-          <Moon className="cursor-pointer" />
+          <Button variant="link" onClick={toogleTheme}>
+            {isDark ? <Moon className="cursor-pointer" /> : <Sun className="cursor-pointer"/>}
+          </Button>
 
           <div className="flex items-center gap-3">
             <div className="flex flex-col leading-tight">
